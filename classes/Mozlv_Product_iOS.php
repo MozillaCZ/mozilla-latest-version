2bb4c37a22361b6a3f72c9dece99cd3b934e14dd
Michal Stanke <michal@stanke.cz>
2016-06-28 22:17:27 +0200

    Add support for iOS
    fix https://github.com/MozillaCZ/mozilla-latest-version/issues/32

---
+++ classes/Mozlv_Product_iOS.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Firefox for iOS product class.
+ * 
+ * @author Michal Stanke <michal.stanke@mikk.cz>
+ */
+class Mozlv_Product_iOS extends Mozlv_Product_Class {
+
+	protected $resource_URL = 'https://product-details.mozilla.org/mobile_versions.json';
+	protected $channel_to_resource_index = array (
+										'release' => 'ios_version',
+										'beta' => 'ios_beta_version',
+									);
+	// %1$s will be replaced by the product version
+	// %2$s will be replaced by the language
+	// %3$s will be replaced by the platform
+	protected $download_URL = 'https://itunes.apple.com/%2$s/app/firefox-web-browser/id989804926';
+	protected $langpack_URL = '';
+	protected $changelog_URL = 'https://www.mozilla.org/%2$s/ios/%1$s/releasenotes/';
+	protected $requirements_URL = 'https://support.mozilla.org/kb/firefox-available-iphone-or-ipad-my-language';
+
+	protected function get_loader() {
+		return Mozlv_Loader_JSON::getInstance();
+	}
+
+}
