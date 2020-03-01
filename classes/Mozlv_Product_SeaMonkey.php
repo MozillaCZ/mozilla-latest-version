418e5dbcade4145a0d2cb3bd1e1f9c7066b9516b
Michal Stanke <michal@stanke.cz>
2020-03-01 15:49:26 +0100

    New SeaMonkey download links

--- classes/Mozlv_Product_SeaMonkey.php
+++ classes/Mozlv_Product_SeaMonkey.php
@@ -16,7 +16,7 @@ class Mozlv_Product_SeaMonkey extends Mozlv_Product_Class {
 	// %2$s will be replaced by the language
 	// %3$s will be replaced by the platform
 	protected $download_URL = 'https://download.mozilla.org/?product=seamonkey-%1$s&os=%3$s&lang=%2$s';
-	protected $langpack_URL = 'https://ftp.mozilla.org/pub/seamonkey/releases/%1$s/win32/xpi/%2$s.xpi';
+	protected $langpack_URL = 'https://ftp.mozilla.org/pub/seamonkey/releases/%1$s/langpack/seamonkey-%1$s.%2$s.langpack.xpi';
 	protected $changelog_URL = 'https://www.seamonkey-project.org/releases/seamonkey%1$s/';
 	protected $requirements_URL = 'https://www.seamonkey-project.org/doc/system-requirements';
 
@@ -28,15 +28,15 @@ class Mozlv_Product_SeaMonkey extends Mozlv_Product_Class {
 		if ( $url == $this->download_URL ) {
 			switch ( $platform ) {
 				case 'win':
-					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/win32/%2$s/seamonkey-%1$s.installer.exe', $channel, $platform );
+					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/win32/%2$s/seamonkey-%1$s.%2$s.win32.installer.exe', $channel, $platform );
 				case 'win64':
-					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/win64/%2$s/seamonkey-%1$s.installer.exe', $channel, $platform );
+					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/win64/%2$s/seamonkey-%1$s.%2$s.win64.installer.exe', $channel, $platform );
 				case 'mac':
-					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/mac/%2$s/seamonkey-%1$s.dmg', $channel, $platform );
+					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/mac/%2$s/seamonkey-%1$s.%2$s.mac.dmg', $channel, $platform );
 				case 'lin':
-					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/linux-i686/%2$s/seamonkey-%1$s.tar.bz2', $channel, $platform );
+					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/linux-i686/%2$s/seamonkey-%1$s.%2$s.linux-i686.tar.bz2', $channel, $platform );
 				case 'lin64':
-					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/linux-x86_64/%2$s/seamonkey-%1$s.tar.bz2', $channel, $platform );
+					return parent::get_latest_url( 'https://archive.mozilla.org/pub/seamonkey/releases/%1$s/linux-x86_64/%2$s/seamonkey-%1$s.%2$s.linux-x86_64.tar.bz2', $channel, $platform );
 			}
 		}
 		return parent::get_latest_url( $url, $channel, $platform );
